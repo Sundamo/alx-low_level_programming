1c4fadc271deb70a9fbc00623e8f515d9545268a
Sundamo <sundamo1993@gmail.com>
2022-09-10 06:31:44 -0700

    task 4 corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main-use put function to prin the string
+ * main-use puts function to prin the string
  *
  * the program prints "Programming is like building a multilinqual puzzle"
  * return: 0
  */
 int main(void)
 {
-	puts("Programming is like building a multinlingual puzzle"\n");
+	puts("\"Programming is like building a multinlingual puzzle");
 	return (0);
 }
