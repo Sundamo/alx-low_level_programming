33bb3acca7cce886a9b17262cb567673ee9a9096
Sundamo <sundamo1993@gmail.com>
2022-09-10 07:04:06 -0700

    i don tire

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - use puts function to prin the string
+ * main - print the string in put function
  *
  *Description: using puts function
  * the program prints "Programming is like building a multilinqual puzzle"
