76e707dfd3305e31116b39bc764db72f8b04bcea
Sundamo <sundamo1993@gmail.com>
2022-09-10 06:35:38 -0700

    task 4 done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
 /**
  * main-use puts function to prin the string
  *
+ *Description: using puts function
  * the program prints "Programming is like building a multilinqual puzzle"
  * return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multinlingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
