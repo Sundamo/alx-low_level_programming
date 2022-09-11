6bf664743135f73ab6bf784bac6fffae3bdf4feb
Sundamo <sundamo1993@gmail.com>
2022-09-10 18:54:36 -0700

    task 6 done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 
 	printf("Size of a char: 1 byte(s)\n", sizeof(c));
 	printf("Size of an int: 4 byte(s)\n", sizeof(i));
-	printf("Size of a long int: 8 byte(s)\n", sizeof(li));
+	printf("Size of a long int: 4 byte(s)\n", sizeof(li));
 	printf("Size of a long long int: 8 byte(s)\n", sizeof(lli));
 	printf("Size of a float: 4 byte(s)\n", sizeof(f));
 	return (0);
