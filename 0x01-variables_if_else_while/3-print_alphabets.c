e43e74c47a9bcf274c6a9019f3124e2ee56a0a35
Sundamo <sundamo1993@gmail.com>
2022-09-12 03:10:47 -0700

    task 3 done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - print text
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+	}
+	for (low = 'A'; low <= 'Z'; low++)
+	{
+		putchar(low);
+	}
+	putchar('\n');
+
+	return (0);
+}
