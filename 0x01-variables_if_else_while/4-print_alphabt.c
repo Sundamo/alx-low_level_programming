f6304a141d8fb5bd4167c02c31b2cdc2015b0514
Sundamo <sundamo1993@gmail.com>
2022-09-12 03:18:52 -0700

    task 4 done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - print alphabetic in lower case
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char low, e, q;
+
+	e='e';
+	q='q';
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+	}
+	putchar('\n');
+
+	return (0);
+}
