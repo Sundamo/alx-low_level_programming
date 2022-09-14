02268f3d03d63e8cefc709acd5dda133ca7b4ba8
Sundamo <sundamo1993@gmail.com>
2022-09-14 13:07:01 -0700

    task abs done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Check code
+ * @r: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: 0
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+	}
+}
