d892ab333e4b18fc509ed77a1cc81f6f64205c25
Sundamo <sundamo1993@gmail.com>
2022-09-14 11:05:37 -0700

    task 1 alph completed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ * main - print alphabet
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+	return (0);
+}
