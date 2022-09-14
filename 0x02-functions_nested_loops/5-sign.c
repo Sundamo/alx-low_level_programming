26fb06c549b1fdae1e17c0ce19b3daa10ce17f30
Sundamo <sundamo1993@gmail.com>
2022-09-14 12:59:03 -0700

    task sign completed

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - Check code
+ * @n: An input number
+ *  Return: 0
+ */
+int print_sign(int n)
+{
+	int value;
+
+	if (n > 0)
+	{
+		value = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		value = 0;
+		_putchar('0');
+	}
+	else
+	{
+		value = -1;
+		_putchar('-');
+	}
+
+	return (value);
+}
