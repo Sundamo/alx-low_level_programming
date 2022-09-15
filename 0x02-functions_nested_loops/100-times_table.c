a015387524142c1669f0e346da1e920d03c68969
Sundamo <sundamo1993@gmail.com>
2022-09-14 19:15:00 -0700

    heyy

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,38 +18,29 @@ void print_times_table(int n)
 				putformat(i * j);
 			_putchar('\n');
 		}
-	}
-}
-
-/**
- * putformat - formatted characters to output
- * @n: number to format
- * Return: 0
- */
-void putformat(int n)
-{
-	if (n <= 9)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	else if (n > 9 && n <= 99)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
+		if (n <= 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(n + '0');
+		}
+		else if (n > 9 && n <= 99)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
+		else
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(n / 100 + '0');
+			_putchar(n / 10 % 10 + '0');
+			_putchar(n % 10 + '0');
+		}
 	}
 }	
