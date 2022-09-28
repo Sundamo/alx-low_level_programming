abedaf1be7f5be48c4a01197491550cf9b2be2c2
Sundamo <sundamo1993@gmail.com>
2022-09-27 19:04:54 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,13 +10,13 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < n)
+	/*Delacring FOR*/
+	for (i = 0; i < n; i++)
 	{
-		*(s + 1) = b;
-		i++;
-	}
+		*(s + i) = b; /*add 1 position s*/
+	} /*END FOR*/
 
 	return (s);
 }
