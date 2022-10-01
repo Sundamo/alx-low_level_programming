8df628c0fb9d3edc0c74e1382268b4fed080a2cf
Sundamo <sundamo1993@gmail.com>
2022-09-30 19:58:44 -0700

    task o

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,41 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+
+void print_string(char *str);
 
 /**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
+ * main - Prints its name
  *
- * Return: Always 0
+ * @argc: Length of argv
+ *
+ * @argv: Arrays of strings of the program arguments
+ *
+ * Return: 0, Success
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
+	print_string(argv[0]);
+	putchar('\n');
 
 	return (0);
+}
+
+/**
+ * print_string - Prints all char of a string
+ *
+ * @str: Pointer to string
+ *
+ * Return: void
+ */
+
+void print_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
 }
