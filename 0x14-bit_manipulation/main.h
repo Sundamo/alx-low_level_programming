b21eadd3b5c4722d072ea38fc7703337a29dacf8
Sundamo <sundamo1993@gmail.com>
2022-11-08 18:52:45 -0800

    main.h correct

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
+/**
  * File: main.h
  * Desc: Header file containing prototypes for all functions
- * 	written in the 0x13-bit_manipulation directory.
+ * written in the 0x13-bit_manipulation directory.
  */
 
 int _putchar(char c);
