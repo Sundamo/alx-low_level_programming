8001d802ba2f3e9c27b5f3d01db7872712ad030e
Sundamo <sundamo1993@gmail.com>
2023-08-15 16:29:53 -0700

    task one done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - write a program
+ *
+ * description - a program that ptints _putchar
+ *
+ * return 0
+ */
+
+int _putchar;
+{
+	_putchar("_putchar");
+	 _putchar("\n);
+
+	return (0);
+}
+
